edb7a657ba554608c5dc824c3f520d506d38b308
unknown <artemis.ntountou@gmail.com>
2023-12-30 19:38:56 +0200

    complete conversation using images

--- app.py
+++ app.py
@@ -7,24 +7,25 @@ from pathlib import Path
 from dotenv import load_dotenv
 from PIL import Image
 
-from image_functions import initialize_chat, new_question
+from image_functions import initialize_chat, new_question_to_chat
 
 with st.sidebar:
-    st.title("Useful Information")
+    st.title("🎉 Image Chatbot Extravaganza! 💬")
     st.markdown('''
-    - This is the Chatbot level 1 in order to better understand how they work
-    - I use gpt-35-turbo and text-embedding-ada-002 of Azure OpenAI due to low cost
-    - Write the index name
-        - if it does not already exist you can select the image_file that you want to use for chatting
-        - else it will load the embeddings from disk and use the image_file that you have chosen the previous time
-    - [Langchain](https://www.langchain.com/)
+    - Upload images, get enchanting descriptions, and insightful answers.
+    - It is used gpt-4 with vision of Azure OpenAI
+    - Provide an Image
+    - Ask something about the image and then continue chatting if you feel so
+    - ⏳ Please be patient; complex questions may take a moment to answer
+    - 🚀 Set Sail on the Chatbot Adventure
+    - [Azure Vision](https://learn.microsoft.com/en-us/azure/ai-services/openai/how-to/gpt-with-vision)
     - [Streamlit](https://streamlit.io/)'''
                 )
 
 
 def main():
     # Display app header
-    st.header("Chat using an Image 🤖💬")
+    st.header("Chat using an Image 🤖🖼️")
 
     # Placeholder for uploaded image_file
     image_file = None
@@ -40,10 +41,11 @@ def main():
         # Create a placeholder for the file uploader
         file_uploader_placeholder = st.empty()
 
-        image_file = file_uploader_placeholder.file_uploader("Upload your Image", type=['png', 'jpg'], accept_multiple_files=False)
+        image_file = file_uploader_placeholder.file_uploader("Upload your Image", type=['png', 'jpg'],
+                                                             accept_multiple_files=False)
 
     if initial_text is None:
-        #create a placeholder to write text
+        # create a placeholder to write text
         initial_text_placeholder = st.empty()
 
         # Write first question
@@ -55,53 +57,56 @@ def main():
             value=None
         )
 
-
     # it will not run until they provide an image
-    if image_file:
-        # Read image file as BytesIO
-        # with open('santaclaus.jpg', 'rb') as file:
-        image_bytes = BytesIO(image_file.read())
+    if image_file is not None:
 
-        # Decode image using PIL
-        img = Image.open(image_bytes)
+        file_uploader_placeholder.empty()
 
-        # Show uploaded image
-        st.image(img, caption="Uploaded Image", use_column_width=True)
-
-        # Initialize chat
-        if message == 0 and initial_text is not None:
-
-            file_uploader_placeholder.empty()
+        if initial_text is not None:
 
             initial_text_placeholder.empty()
 
-            # Initialize streamlit chat
-            st.session_state.messages = [{"role": "user", "content": initial_text}]
+            # Read image file as BytesIO
+            # with open('santaclaus.jpg', 'rb') as file:
+            image_bytes = BytesIO(image_file.read())
 
-            # Initialize chat and get response from model
-            response, payload, headers = initialize_chat(image_bytes, initial_text)
-            message += 1
+            # Decode image using PIL
+            img = Image.open(image_bytes)
 
-            # Update chat history with assistant's response
-            st.session_state.messages.append({"role": "assistant", "content": response.json()['choices'][0]['message']['content']})
+            # Show uploaded image
+            st.image(img, caption="Uploaded Image", use_column_width=True)
 
-        # Display existing chat history
-        if message > 0:
-            for msg in st.session_state.messages:
-                st.chat_message(msg["role"]).write(msg["content"])
+            # Initialize chat
+            if message == 0 and initial_text is not None:
 
-        # Get user input
-        if query := st.chat_input() and message > 0:
+                # Initialize streamlit chat
+                st.session_state.messages = [{"role": "user", "content": initial_text}]
 
-            # Write new message
-            st.session_state.messages.append({"role": "user", "content": query})
-            st.chat_message("user").write(query)
+                # Initialize chat and get response from model
+                response, payload, headers = initialize_chat(image_bytes, initial_text)
+                message += 1
 
-            # Get response from chat-gpt
-            response, payload = new_question(payload, query, headers)
-            # Update chat history with assistant's response
-            st.session_state.messages.append({"role": "assistant", "content": response.json()['choices'][0]['message']['content']})
-            st.chat_message("assistant").write(response.json()['choices'][0]['message']['content'])
+                # Update chat history with assistant's response
+                st.session_state.messages.append({"role": "assistant", "content": response})
+
+            # Display existing chat history
+            if message > 0:
+                for msg in st.session_state.messages:
+                    st.chat_message(msg["role"]).write(msg["content"])
+
+            # Get user input
+            if query := st.chat_input():
+
+                # Write new message
+                st.session_state.messages.append({"role": "user", "content": query})
+                st.chat_message("user").write(query)
+
+                # Get response from chat-gpt
+                response, payload = new_question_to_chat(payload, query, headers)
+
+                # Update chat history with assistant's response
+                st.session_state.messages.append({"role": "assistant", "content": response})
+                st.chat_message("assistant").write(response)
 
 
 if __name__ == '__main__':
--- image_functions.py
+++ image_functions.py
@@ -4,7 +4,6 @@ import requests
 
 # function in order to initialize chat, it will need an image and some text
 def initialize_chat(image_bytes, starting_text):
-
     GPT4_KEY = os.environ.get('OPENAI_API_KEY')
     GPT4_ENDPOINT = os.environ.get('OPENAI_API_BASE')
     GPT4_NAME = os.environ.get('LLM4_NAME')
@@ -60,14 +59,17 @@ def initialize_chat(image_bytes, starting_text):
     except requests.RequestException as e:
         raise SystemExit(f"Failed to make the request. Error: {e}")
 
-    return response, payload, headers
+    # add response to chat history
+    assistant_response, payload = add_assistant_reply(response, payload)
+
+    return assistant_response, payload, headers
 
 
 # ask for new information about the image
-def new_question(payload, question, headers):
-
-    GPT4_KEY = os.environ.get('LLM4_NAME')
+def new_question_to_chat(payload, question, headers):
     GPT4_ENDPOINT = os.environ.get('OPENAI_API_BASE')
+    GPT4_NAME = os.environ.get('LLM4_NAME')
+    GPT4_VERSION = os.environ.get('OPENAI_API_VERSION')
 
     # Append user's message to the conversation
     user_message = {
@@ -81,11 +83,36 @@ def new_question(payload, question, headers):
     }
     payload["messages"].append(user_message)
 
+    GPT4V_ENDPOINT = f"{GPT4_ENDPOINT}openai/deployments/{GPT4_NAME}/chat/completions?api-version={GPT4_VERSION}"
+
     # Send another request with appended message
     try:
-        response = requests.post(GPT4_ENDPOINT, headers=headers, json=payload)
+        response = requests.post(GPT4V_ENDPOINT, headers=headers, json=payload)
         response.raise_for_status()  # Will raise an HTTPError if the HTTP request returned an unsuccessful status code
     except requests.RequestException as e:
         raise SystemExit(f"Failed to make the request. Error: {e}")
 
-    return response, payload
+    # add response to chat history
+    assistant_response, payload = add_assistant_reply(response, payload)
+
+    return assistant_response, payload
+
+
+# In order to add assistant response to payload and maintain conversation history
+def add_assistant_reply(response, payload):
+    # Extract assistant's response from the API response
+    assistant_response = response.json()["choices"][0]["message"]["content"]
+
+    # Append assistant's message to the conversation
+    assistant_message = {
+        "role": "assistant",
+        "content": [
+            {
+                "type": "text",
+                "text": assistant_response
+            }
+        ]
+    }
+    payload["messages"].append(assistant_message)
+
+    return assistant_response, payload
